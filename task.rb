97a41e8a237b885040fdeffa4d3fbebf5fbe712b
Yumiko Tezuka <ytezuka5@gmail.com>
2020-08-12 16:26:37 +0900

    ruby基礎課題6~8

--- task.rb
+++ task.rb
@@ -6,7 +6,7 @@ def q1
 
   # 以下に回答を記載
   names<<"斎藤"
-  puts names[4]
+  p names[4]
 end
 
 def q2
@@ -51,14 +51,16 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+ numbers2 = numbers1.map{|n|n*10}
+ p numbers2
+  
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -67,7 +69,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
